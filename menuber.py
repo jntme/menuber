2c08004653b2da551678452e2b2e6725681dd505
jntme <mail@jntme.ch>
2016-11-07 22:23:43 +0100

    add exception handling & testing

--- menuber.py
+++ menuber.py
@@ -1,7 +1,7 @@
 
 class Menuber:
 
-    def __init__(self, menuTitle, menuFunctionMap):
+    def __init__(self, menuTitle, menuFunctionMap, subMenu=None):
 
         self.menuTitle = menuTitle
         self.menuFunctionMap = menuFunctionMap
@@ -10,12 +10,26 @@ class Menuber:
     def menu(self):
         print(self.menuTitle)
         print(len(self.menuTitle)*"=")
+        print("")
 
         for entry in self.menuFunctionMap:
             print(entry, "\t", self.menuFunctionMap[entry])
 
+        #user_input = input(">> ")
+
+        #print(user_input)
+
     def callMenuPoint(self, menuPoint):
         if menuPoint not in self.menuFunctionMap:
-            pass # RAISE ERROR!
+            raise MenuPointNotExistingError("No such menu point in map!")
         else:
             return self.menuFunctionMap[menuPoint]()
+
+class Error(Exception):
+    '''Base class for exceptions in this module.'''
+    pass
+
+class MenuPointNotExistingError(Error):
+
+    def __init__(self, message):
+        self.message = message
--- tests/test_simple.py
+++ tests/test_simple.py
@@ -1,6 +1,6 @@
 import unittest2 as unittest
 
-from menuber import Menuber
+from menuber import *
 
 #test functions
 #######################################
@@ -13,17 +13,21 @@ def testFunc2(a, b):
 
 class TestMenuber(unittest.TestCase):
     def test_newMenuberNaming(self):
-        new_menu = Menuber.Menuber("Testname", {1: "Test1", 2: "Test2"})
+        new_menu = Menuber("Testname", {1: "Test1", 2: "Test2"})
 
         self.assertEqual(new_menu.menuTitle, "Testname")
         self.assertEqual(new_menu.menuFunctionMap[1], "Test1")
 
     def test_printMenu(self):
-        new_menu = Menuber.Menuber("Testname", {1: testFunc1, 2: testFunc2(1,2)})
+        new_menu = Menuber("Testname", {1: testFunc1, 2: testFunc2(1, 2)})
 
         new_menu.menu()
 
     def test_newMenuberFunctionCalling(self):
-        new_menu = Menuber.Menuber("Testname", {1: testFunc1})
+        new_menu = Menuber("Testname", {1: testFunc1})
 
         self.assertEqual(new_menu.callMenuPoint(1), 1337);
+
+        #should raise MenuPointNotExistingError if not existing
+        self.assertRaises(MenuPointNotExistingError,
+                          new_menu.callMenuPoint, 2)
